1a5f165bde73fa8799c95b56732341e072f70384
gagekta <gc.99hockey@gmail.com>
2023-12-12 14:00:13 -0500

    fixing nicks oopsies

--- Final Project Website/resources/taco.js
+++ Final Project Website/resources/taco.js
@@ -2,34 +2,51 @@
 const appetizers = [
     { name: 'Onion Rings', price: 8 },
     { name: 'Mozzarella Sticks', price: 6 },
+    { name: 'Spinach & Artichoke Dip', price: 5.50 },
+    { name: 'Empanadas', price: 3.45 },
+    { name: 'Quesadilla Bites', price: 4.45 },
 ];
 
 const tacos = [
     { name: 'Taco Salad', price: 8 },
     { name: 'Taco Supreme', price: 10 },
+    { name: 'Soft Taco', price: 4.55 },
+    { name: 'Vegan Taco', price: 8.50 },
+    { name: 'Fish Taco', price: 1.30 },
+    { name: 'Tacos From Outer-Space', price: 6.80 },
 ];
 
 const entrees = [
     { name: 'Salmon Ravioli', price: 12 },
     { name: 'Chicken Chicken Chicken What Combo Are you Pickin\'', price: 15 },
+    { name: 'Enchiladas', price: 10.30 },
+    { name: 'Chimichanga', price: 18.25 },
+    { name: 'TacoBoutIT Bread Bowl', price: 11.50 },
 ];
 
 const desserts = [
     { name: 'Salmon Dessert', price: 7.99 },
     { name: 'Chocolate Surprise', price: 5.85 },
+    { name: 'Churro', price: 1.50 },
+    { name: 'Fried Iced Cream', price: 5.35 },
+    { name: 'Choco Taco', price: 5.00 },
+    { name: 'Tres Leches Cake', price: 6.90 },
 ];
 
 const drinks = [
     { name: 'Soda', price: 2 },
     { name: 'Iced Tea', price: 3 },
     { name: 'Raspberry Lemonade', price: 2.50 },
+    { name: 'Horchata', price: 4.95 },
+    { name: 'Mexican Coke', price: 4.00 },
+    { name: 'Corona Lite', price: 1.50 },
+    { name: 'Water', price: 0.00 },
 ];
 
+// Global array to store cart items
+const cartItems = [];
 
 document.addEventListener('DOMContentLoaded', function () {
-   
-
-
     // Function to generate the menu based on category
     function generateMenu(category, containerId, categoryName) {
         const menuContainer = document.getElementById(containerId);
@@ -61,47 +78,73 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // Function to add an item to the cart
     function addToCart(name, price) {
-        const cartItemsContainer = document.getElementById('cart-items');
-        
-        // Check if the item is already in the cart
-        const existingCartItem = Array.from(cartItemsContainer.children).find(item => item.dataset.name === name);
+        const cartItemIndex = cartItems.findIndex(item => item.name === name);
 
-        if (existingCartItem) {
+        if (cartItemIndex !== -1) {
             // If the item is already in the cart, increment quantity
-            existingCartItem.dataset.quantity = parseInt(existingCartItem.dataset.quantity) + 1;
-            existingCartItem.textContent = `${name} x${existingCartItem.dataset.quantity} - $${price * existingCartItem.dataset.quantity}`;
+            cartItems[cartItemIndex].quantity++;
         } else {
             // If the item is not in the cart, add a new item
-            const cartItem = document.createElement('li');
-            cartItem.dataset.name = name;
-            cartItem.dataset.quantity = 1;
-            cartItem.textContent = `${name} x${cartItem.dataset.quantity} - $${price * cartItem.dataset.quantity}`;
-            cartItemsContainer.appendChild(cartItem);
+            cartItems.push({ name, price, quantity: 1 });
         }
 
         // Update total value
         updateTotalValue();
+
+        // Update cart display
+        displayCartItems();
     }
 
     // Function to list the total value of items in the cart
     function updateTotalValue() {
-        const cartItemsContainer = document.getElementById('cart-items');
         const totalValueElement = document.getElementById('total-value');
 
         let totalValue = 0;
 
         // Calculate total value by iterating through cart items
-        Array.from(cartItemsContainer.children).forEach(item => {
-            const price = parseFloat(item.textContent.match(/\$\d+(\.\d{1,2})?/)[0].slice(1)); // Locates the price of the item through the array to the 2nd decimal place.
-            
-            totalValue += price;
+        cartItems.forEach(item => {
+            totalValue += item.price * item.quantity;
         });
 
         // Round the total value to two decimal places
         totalValue = parseFloat(totalValue.toFixed(2));
 
-        // Tells the HTML what number to show for the total value
+        // Update the HTML element displaying total value
         totalValueElement.textContent = totalValue.toFixed(2);
+    }
+
+    // Function to display cart items
+    function displayCartItems() {
+        const cartItemsContainer = document.getElementById('cart-items');
+
+        // Clear the existing content
+        cartItemsContainer.innerHTML = '';
+
+        // Iterate through cart items and create HTML elements
+        cartItems.forEach(item => {
+            const cartItem = document.createElement('li');
+            cartItem.textContent = `${item.name} x${item.quantity} - $${(item.price * item.quantity).toFixed(2)}`;
+
+            // Add a button to remove the item from the cart
+            const removeButton = document.createElement('button');
+            removeButton.textContent = 'x';
+            removeButton.addEventListener('click', () => removeCartItem(item));
+
+            cartItem.appendChild(removeButton);
+            cartItemsContainer.appendChild(cartItem);
+        });
+    }
+
+    // Function to remove an item from the cart
+    function removeCartItem(item) {
+        // Set the quantity to zero
+        item.quantity = 0;
+
+        // Update total value
+        updateTotalValue();
+
+        // Update cart display
+        displayCartItems();
     }
 
     // Generate the menu for each category
@@ -110,9 +153,7 @@ document.addEventListener('DOMContentLoaded', function () {
     generateMenu(entrees, 'menu', 'Entrees');
     generateMenu(desserts, 'menu', 'Desserts');
     generateMenu(drinks, 'menu', 'Drinks');
-    
 });
-
 
 function addItem() {
     const category = document.getElementById('category').value;
@@ -226,30 +267,102 @@ function getCategoryName(index) {
     return categoryNames[index];
 }
 
+
+
+
 // Initial update
 updateMenu();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
         
+
+// Function to generate HTML content for the receipt
+function generateReceiptContent() {
+    const receiptContent = document.getElementById('receiptContent');
+    receiptContent.innerHTML = '';
+
+    cartItems.forEach(item => {
+        const itemDiv = document.createElement('div');
+        itemDiv.textContent = `${item.name} x${item.quantity} - $${(item.price * item.quantity).toFixed(2)}`;
+        receiptContent.appendChild(itemDiv);
+    });
+
+    // Calculate and display total
+    const totalValue = cartItems.reduce((total, item) => total + item.price * item.quantity, 0);
+    document.getElementById('receiptTotal').textContent = totalValue.toFixed(2);
+}
+
+function openCartPopup1() {
+    const overlay = document.getElementById('overlay');
+    const cartPopup1 = document.getElementById('cartPopup1');
+
+    overlay.style.display = 'block';
+    cartPopup1.style.display = 'block';
+}
+
+function closeCartPopup1() {
+    const overlay = document.getElementById('overlay');
+    const cartPopup1 = document.getElementById('cartPopup1');
+
+    overlay.style.display = 'none';
+    cartPopup1.style.display = 'none';
+}
+
+function openReceiptPopup() {
+    const overlay = document.getElementById('overlay');
+    const receiptPopup = document.getElementById('receiptPopup');
+
+    overlay.style.display = 'block';
+    receiptPopup.style.display = 'block';
+
+    // Generate and display receipt content
+    generateReceiptContent();
+}
+
+function closeReceiptPopup() {
+    const overlay = document.getElementById('overlay');
+    const receiptPopup = document.getElementById('receiptPopup');
+
+    overlay.style.display = 'none';
+    receiptPopup.style.display = 'none';
+}
+
+function openCardPaymentPopup() {
+    const overlay = document.getElementById('overlay');
+    const cardPaymentPopup = document.getElementById('cardPaymentPopup');
+
+    overlay.style.display = 'block';
+    cardPaymentPopup.style.display = 'block';
+}
+
+function closeCardPaymentPopup() {
+    const overlay = document.getElementById('overlay');
+    const cardPaymentPopup = document.getElementById('cardPaymentPopup');
+
+    overlay.style.display = 'none';
+    cardPaymentPopup.style.display = 'none';
+}
+
+// Function to handle payment selection
+function selectPayment(paymentType) {
+    if (paymentType === 'Card') {
+        openCardPaymentPopup();
+        closeCartPopup1();
+    } else if (paymentType === 'PayAtStore') {
+        openReceiptPopup();
+        closeCartPopup1();
+    }
+}
+function processCardPayment() {
+    const cardHolderName = document.getElementById('cardHolderName').value;
+    const cardNumber = document.getElementById('cardNumber').value;
+    const expirationDate = document.getElementById('expirationDate').value;
+
+ 
+    closeCardPaymentPopup();
+    openReceiptPopup();
+}
+
+
 
 
 
